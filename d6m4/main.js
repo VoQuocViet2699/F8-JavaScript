3d8b5acb73d67d3eac4c22f201264067e64fa4a0
voquocvietdz <voquocviet2699@gmail.com>
2024-04-06 21:52:45 +0700

    them du lieu moi vao main.js

---
+++ d6m4/main.js
@@ -0,0 +1,42 @@
+// alert("hello everyone!!!!")
+
+// prompt("how are you?")
+
+// confirm(" Are you sure you want to")
+
+// setTimeout(() => {
+//     console.log("hello  world!")     
+// }, 1000);
+
+// var timeout = prompt("enter timeout")
+// setInterval(() => {
+//     console.log("hello world!")
+// }, timeout);
+
+//khai bao va goi funcion
+// var myFunction = function () {
+//     console.log("hello world!")
+// }
+// myFunction();
+
+//tao va goi object
+// var myObject = {
+//     namme: "vo quoc viet",
+//     age: 18,
+//     gender: "male",
+//     job: "programmer",
+//     hobby: ["sing", "dancing", "reading"],
+//     hobbies: ["sing", "dancing", "reading"],
+//     hobbies: ["sing", "dancing", "reading"],
+// }
+var a =1 , b =2 ;
+var result = "a" && "b" && "c" && "d" && "e" ;
+
+if(false) {
+    console.log("dung");
+}else {
+    console.log("khong dung");
+}
+
+console.log(result);
+
